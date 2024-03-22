3e056655522578abfa92add49e3bb43b5312aa75
alamgir20 <45663124+alamgir20@users.noreply.github.com>
2024-03-22 20:41:23 +0600

    file upload

--- js/main.js
+++ js/main.js
@@ -1,43 +1,9 @@
-// cursor js
-const magnet = document.querySelector('.magnet');
-const cursor = document.querySelector('.magnet-cursor');
-const circle = document.querySelector('.magnet-circle');
-let [startX,startY,endX,endY] = [0,0,0,0];
-let time = 0.1;
-let raf;
-
-const lerp = (start,end,t) => start * (1-t) + end * t;
-
-function updateCircle() {
-  startX = lerp(startX,endX,time);
-  startY = lerp(startY,endY,time);
-  circle.style.transform = `translate(${startX}px,${startY}px)`;
-  raf = requestAnimationFrame(updateCircle);
-  startX.toFixed(1) === endX.toFixed(1) && cancelAnimationFrame(raf);
-}
-
-function trackCircle(e) {
-  endX = e.clientX - (circle.clientWidth/2);
-  endY = e.clientY - (circle.clientHeight/2);
-  cancelAnimationFrame(raf);
-  raf = requestAnimationFrame(updateCircle);
-}
-
-function trackCursor(e) {
-  cursor.style.setProperty('--x',`${e.clientX - cursor.clientWidth/2}px`);
-  cursor.style.setProperty('--y',`${e.clientY - cursor.clientHeight/2}px`);
-  magnet.classList.remove('reset');
-}
-
-function init(e) {
-  trackCursor(e); 
-  trackCircle(e);
-  return e.target.matches('.magnet-link') && magnet.classList.add('reset');
-}
-
-window.addEventListener('pointermove',init,false);
-window.addEventListener('load',updateCircle,false);
-// end cursor
+  $(document).ready(function(){
+  $('ul li a').click(function(){
+    $('li a').removeClass("active");
+    $(this).addClass("active");
+});
+});
 
 //Scrolling Effect
 $(window).on('scroll', function () {
@@ -48,3 +14,15 @@ $(window).on('scroll', function () {
   }
 });
 
+function openDaySchedule(url) {
+  // See all options https://dayschedule.com/widget
+  daySchedule.initPopupWidget({
+    url: url,
+    color: {
+      primary: '#232325',
+      secondary: '#adadad',
+    },
+  });
+}
+
+new WOW().init();
